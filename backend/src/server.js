94837f4c19d8f248314166e2683841227d68bd63
GamalElsadany <gamalelsadany2468@gmail.com>
2025-11-24 08:47:36 -0800

    update-profile

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,39 +5,30 @@ import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js'
 import { connectDB } from './lib/db.js'
 import { ENV } from './lib/env.js'
+import cookieParser from "cookie-parser";
 
 
 const app = express();
+app.use(cookieParser()); 
+app.use(express.json()); // أهم سطر
 
-const __dirname= path.resolve();
-app.use(express.json())
-app.use("/api/auth",authRoutes)
-app.use("/api/messages",messageRoutes)
+const __dirname = path.resolve();
 
-//make ready for develoment 
+const PORT = ENV.PORT || 3000;
 
-if(ENV.MODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-    app.get("*",(_,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
-    })
+// make ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-const PORT= ENV.PORT || 3000
 
 app.listen(PORT,()=> {
      console.log("Server ruuning on port: "+PORT)
