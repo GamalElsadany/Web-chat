760c625bdf519fc96b274f4af0205b8e4b61c3a6
GamalElsadany <gamalelsadany2468@gmail.com>
2025-11-24 10:26:40 -0800

    edit-3

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,5 @@
 import User from "../../models/user.js";
-import Message from "../models/message.js";
+import Message from "../models/Message.js";
 
 
 export const getAllContacts = async (req, res) => {
